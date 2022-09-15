cd7f003acbff8af5ab9ddf85a8589bc6f65f95d1
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-14 20:29:24 -0700

    update validations - true

--- app/models/beast.rb
+++ app/models/beast.rb
@@ -5,6 +5,9 @@ class Beast < ApplicationRecord
   validates :age, presence: true
   validates :age, numericality: { only_integer: true, greater_than: 0 }
   validates :desc, presence: true
+  validates :strength, presence: true
+  validates :strength, numericality: { only_integers: true, greater_than: 0 }
+  validates :weapon, presence: true
 
   def self.search(search)
     if search
